822e82ce56ea1747dab2a011be80702284d40dab
alanLingZHANG <133839456+alanLingZHANG@users.noreply.github.com>
2023-11-23 02:41:10 +0000

    speech added

--- chatgpt.py
+++ chatgpt.py
@@ -1,15 +1,44 @@
 import os
 import sys
-
 import openai
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
+import time
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def takeCommandCMD():
+    query = input("Please tell me how can I help you?\n")
+    return query
+
+def takeCommandMIC():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing......")
+        query = r.recognize_google(audio, language="en-AU")
+        print("User Said : " + query)
+
+    except Exception as e:
+        print(e)
+        speak("Say that Again Please.....")
+        return "None"
+    return query
 
 import constants
 
@@ -20,33 +49,38 @@ PERSIST = False
 
 query = None
 if len(sys.argv) > 1:
-  query = sys.argv[1]
+    query = sys.argv[1]
 
 if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    print("Reusing index...\n")
+    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+    loader = DirectoryLoader("data/")
+    if PERSIST:
+        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+    else:
+        index = VectorstoreIndexCreator().from_loaders([loader])
 
 chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 
 chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
 
-  chat_history.append((query, result['answer']))
-  query = None
+while True:
+    if not query:
+        # Choose one of the following methods based on user preference
+         query = takeCommandCMD()  # Uncomment this line if you want to use command line input
+        # query = takeCommandMIC()  # Uncomment this line if you want to use microphone input
+
+    if query.lower() in ['quit', 'q', 'exit']:
+        sys.exit()
+
+    result = chain({"question": query, "chat_history": chat_history})
+    print(result['answer'])
+    speak(result['answer'])
+
+    chat_history.append((query, result['answer']))
+    query = None
